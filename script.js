7a32ff33a8d9cb0b2a5cb5c1cf56ff05b46b67b3
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-10 23:01:48 +0300

    les06

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
 'use strict';
 let freeNumber;
 
-
-/*const isNumber = function (num) {
-   return !isNaN(parseFloat(num)) && isFinite(num);
-
-};
-do {*/
 freeNumber = prompt('Can you gass my favorite number from 1 to 100?');
-/*}
-while (!isNumber(freeNumber));
-
-//while (freeNumber > 100 || freeNumber < 1) {
-// freeNumber = prompt('Can you gass my favorite number from 1 to 100?');
-//}*/
-
 
 const lessNumber = function (a) {
    let number = 56;
+   
    if (a > number) {
       alert('The number is more');
       a = prompt('Can you gass my favorite number less?');
       return a;
-   } else if (a < number && a !== null && a !== '') {
+   } 
+   else if (a < number && a !== null && a !== '') {
       alert('The number is less');
       a = prompt('Can you gass my favorite number more?');
       return a;
-   } else if (a == number) {
+   } 
+   else if (a == number) {
       alert('You are win!!!');
       return a;
    }
@@ -34,6 +24,7 @@ const lessNumber = function (a) {
       alert('The game is over');
       return a;
    }
+
    const theEnd = function (a) {
       if (isNaN(+a)) {
          alert('Enter a number');
@@ -44,5 +35,3 @@ const lessNumber = function (a) {
    theEnd(freeNumber);
 };
 lessNumber(freeNumber);
-
-//console.log(+'abx' !== NaN);
